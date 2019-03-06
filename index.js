fe5dc8118c84de790c41dc0f42c3974ec8c85305
Aditya Waruwu <aditya.william96@gmail.com>
2019-03-06 03:47:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber (line, name) {
   line.push(name);
-  return (`Welcome ${name}. You are number ${line} in the line.`)
+  return (`Welcome ${name}. You are number ${line} in line.`)
 }
