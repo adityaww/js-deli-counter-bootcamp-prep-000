3bfec67acf871e4045c4be3030c8a85af65e84bb
Aditya Waruwu <aditya.william96@gmail.com>
2019-03-06 04:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-if(katzDeliLine.length === 0) {
   var message = "The line is currently: "
+  if(katzDeliLine.length === 0) {
+    return "The line is currently empty"
 }
 }
